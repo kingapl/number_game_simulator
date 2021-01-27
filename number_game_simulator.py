d46d3b9f5930d322064d8efa9520ad953cc7cba6
kingapl <kingapluciennik@wp.pl>
2021-01-27 19:13:10 +0100

    Changed window size.

--- number_game_simulator.py
+++ number_game_simulator.py
@@ -6,7 +6,7 @@ class NumberGameSimulator(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.master.geometry("450x500")
+        self.master.geometry("450x550")
         self.master.title("Number Game Simulator")
         self.pack()
         self.create_widgets()
